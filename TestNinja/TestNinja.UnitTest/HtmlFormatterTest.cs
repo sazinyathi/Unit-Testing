666d5f145599ca0bd41139b4684d4cfac4b5df2b
Saz <sazi.nyathi@lexisnexis.co.za>
2020-09-10 09:49:49 +0200

    DEV: Testing Function that return a string

---
+++ TestNinja/TestNinja.UnitTest/HtmlFormatterTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    public class HtmlFormatterTest
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+            var result = formatter.FormatAsBold("abc");
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+        }
+    }
+}
